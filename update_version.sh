daa684f11353f40f1e3cb117475cd48413e5843a
Guillaume DeMengin <gdemengin@noreply.github.com>
2022-07-18 22:26:03 +0200

    update plugins & versions to test

--- update_version.sh
+++ update_version.sh
@@ -17,6 +17,21 @@ if [ ! -e ./${source}/version ]; then
     exit 1
 fi
 
+#url=https://raw.githubusercontent.com/jenkins-infra/jenkins.io/master/content/_data/changelogs/lts.yml
+#{
+#    echo "# lts versions"
+#    echo "# ${url}"
+#    curl -sSLf ${url} |egrep "^- version" | sed 's/- version: //g' | sed 's/"//g' | sed "s/'//g"
+#} > lts.txt
+
+url=https://raw.githubusercontent.com/jenkinsci/jenkins/master/core/src/main/resources/jenkins/install/platform-plugins.json
+{
+    echo "# default plugins suggested in setupwizard"
+    echo "# ${url}"
+    curl -sSLf ${url} | grep '"suggested": true' | sed 's/.*"name": "//g' | sed 's/",.*$//g' | sort
+} > src/plugins.txt
+
+
 NEW_BADGE="[![${source}/versions.txt](https://img.shields.io/badge/jenkins-$(cat ${source}/version)-blue.svg)](${source}/versions.txt)"
 sed "s|\[\!\[${source}/versions.txt\].*$|${NEW_BADGE}|" README.md > README.md.tmp
 mv README.md.tmp README.md
