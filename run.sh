5f7b94dca79b46c6f840f1af47f77a70863613af
Guillaume DeMengin <gdemengin@noreply.github.com>
2022-07-28 23:01:13 +0200

    improve run.sh

--- run.sh
+++ run.sh
@@ -6,8 +6,10 @@ function usage() {
     echo "usage: $0 [options] [--] <entrypoint parameters>"
     echo "options:"
     echo "  --version <version>"
+    echo "  --dump-version-path <relative path>"
     echo "  --keepalive"
     echo "  --standalone"
+    echo "  --workspace <workspace>"
     echo "  --no-cache"
     echo "  --docker-run-arg <arg>"
     echo "  --plugins <plugins file>"
@@ -15,47 +17,40 @@ function usage() {
     echo "  --entrypoint <entrypoint folder>"
 }
 
-KEEPALIVE=false
-STANDALONE=false
 DOCKER_RUN_ARG=
 DOCKER_BUILD_ARG=
 VERSION=lts
+WORKSPACE=$(pwd)/..
 
 while [ $# -gt 0 ]; do
     case "$1" in
         -h|--help) usage; exit 0;;
         --version) VERSION=$2; shift 2;;
-        --keepalive) KEEPALIVE=true; shift;;
-        --standalone) STANDALONE=true; shift;;
+        --dump-version-path) export INPUT_DUMP_VERSION_PATH="\${GITHUB_WORKSPACE}/$2"; shift 2 ;;
+        --keepalive) export INPUT_KEEPALIVE=true; shift ;;
+        --standalone) export INPUT_STANDALONE=true; shift ;;
+        --workspace) WORKSPACE="$2"; shift 2;;
         --no-cache) DOCKER_BUILD_ARG="--pull --no-cache"; shift;;
         --docker-run-arg) DOCKER_RUN_ARG="$2"; shift 2;;
-        --plugins) export INPUT_PLUGINS=$2; shift 2;;
-        --init_groovy) export INPUT_INIT_GROOVY=$2; shift 2;;
-        --entrypoint) export INPUT_ENTRYPOINT=$2; shift 2;;
+        --plugins) export INPUT_PLUGINS="$2"; shift 2;;
+        --init_groovy) export INPUT_INIT_GROOVY="$2"; shift 2;;
+        --entrypoint) export INPUT_ENTRYPOINT="$2"; shift 2;;
         --) shift; break;;
         *) echo "unknown parameter $1"; usage; exit 1;;
     esac
 done
 
-# input params
-export GITHUB_WORKSPACE=/workspace
-export INPUT_DUMP_VERSION_PATH=\${GITHUB_WORKSPACE}/.version
-
-export INPUT_KEEPALIVE=${KEEPALIVE}
-export INPUT_STANDALONE=${STANDALONE}
-
-cd $(dirname $0)/
-./prepare.sh ${VERSION}
-cd .jenkins
+WORKDIR=$(dirname $0)
+${WORKDIR}/prepare.sh ${VERSION}
 
 IMAGE_NAME=jenkins-${VERSION}
-docker build ${DOCKER_BUILD_ARG} -t ${IMAGE_NAME} .
+docker build ${DOCKER_BUILD_ARG} -t ${IMAGE_NAME} ${WORKDIR}/.jenkins/.
 
 set -x
 
 docker run -it --rm \
-  --workdir ${GITHUB_WORKSPACE} \
-  -e GITHUB_WORKSPACE \
+  --workdir /workspace \
+  -e GITHUB_WORKSPACE=/workspace \
   -e CI=false \
-  -v $(pwd)/..:${GITHUB_WORKSPACE} \
+  -v ${WORKSPACE}:/workspace \
   ${DOCKER_RUN_ARG} ${IMAGE_NAME} "$@"
