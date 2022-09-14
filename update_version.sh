6c222eda83277655ae65b02656d44d24253d910d
Guillaume DeMengin <gdemengin@users.noreply.github.com>
2022-09-14 12:28:31 +0200

    update version through pull request

--- update_version.sh
+++ update_version.sh
@@ -6,14 +6,18 @@ set -e
 
 [ $# -eq 1 ] || {
     echo "ERROR wrong number of arguments"
-    echo "usage: $0 <version dir>"
+    echo "usage: $0 <source version dir> <target>"
     exit 1
 }
 
 source=$1
+target=last-good-version
 
-if [ ! -e ./${source}/version ]; then
-    echo "ERROR ./${source}/version missing"
+rm -rf ${target}
+cp -r ${source} ${target}
+
+if [ ! -e ./${target}/version ]; then
+    echo "ERROR ./${target}/version missing"
     exit 1
 fi
 
@@ -32,8 +36,8 @@ url=https://raw.githubusercontent.com/jenkinsci/jenkins/master/core/src/main/res
 } > src/plugins.txt
 
 
-NEW_BADGE="[![${source}/versions.txt](https://img.shields.io/badge/jenkins-$(cat ${source}/version)-blue.svg)](${source}/versions.txt)"
-sed "s|\[\!\[${source}/versions.txt\].*$|${NEW_BADGE}|" README.md > README.md.tmp
+NEW_BADGE="[![${target}/versions.txt](https://img.shields.io/badge/jenkins-$(cat ${target}/version)-blue.svg)](${target}/versions.txt)"
+sed "s|\[\!\[${target}/versions.txt\].*$|${NEW_BADGE}|" README.md > README.md.tmp
 mv README.md.tmp README.md
 
 exit 0
